133263d76839eb9df53f63f84e8f440e348a27da
Jonathan Stalnaker <steamsynthetic@gmail.com>
2023-09-30 17:10:53 -0700

    card updates

--- schemas/home/lowestPrice.js
+++ schemas/home/lowestPrice.js
@@ -1,5 +1,5 @@
 ﻿// schemas/home/lowestPrice.js
-import blockContent from '../blocks/blockContent'
+import card from '../blocks/card'
 
 export default {
   name: 'lowestPriceCards',
@@ -13,34 +13,10 @@ export default {
   fields: [
     {name: 'lowestPriceCardTitle', type: 'string', title: 'Section Title'},
     {
-      ...blockContent,
-      name: 'card_1',
-      title: 'Card #1 Content',
-      options: {
-        collapsible: true,
-        collapsed: true,
-        columns: 1,
-      },
-    },
-    {
-      ...blockContent,
-      name: 'card_2',
-      title: 'Card #2 Content',
-      options: {
-        collapsible: true,
-        collapsed: true,
-        columns: 1,
-      },
-    },
-    {
-      ...blockContent,
-      name: 'card_3',
-      title: 'Card #3 Content',
-      options: {
-        collapsible: true,
-        collapsed: true,
-        columns: 1,
-      },
+      title: 'Star Items',
+      name: 'starItems', // Change the field name
+      type: 'array', // Change the field type to array
+      of: [card],
     },
   ],
 }
