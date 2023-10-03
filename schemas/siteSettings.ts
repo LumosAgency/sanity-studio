37f2bb5d0cd389dfa8f06f9be91cd4d308feb3b8
Jonathan Stalnaker <steamsynthetic@gmail.com>
2023-10-03 08:32:53 -0700

    saving

--- schemas/siteSettings.ts
+++ schemas/siteSettings.ts
@@ -1,12 +1,11 @@
 ﻿// schemas/siteSettings.ts
 import alertBar from './global/alertBar'
 import calculator from './global/calculator'
-import siteLogo from './global/siteLogo'
 
 export default {
   name: 'siteSettings',
   title: 'Glboal Site Settings',
   type: 'document',
-  fields: [siteLogo, alertBar, calculator],
+  fields: [alertBar, calculator],
 }
 
