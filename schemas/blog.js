244bb7e540ff7c5959949960026621fe911d029a
jonnystalnaker <steamsynthetic@gmail.com>
2024-04-17 14:22:22 -0700

    adding blog schema

---
+++ schemas/blog.js
@@ -0,0 +1,65 @@
+﻿// schemas/resourcePages.ts
+import accordion from './blocks/accordion'
+import blockContent from './blocks/blockContent'
+import image from './blocks/image'
+import quote from './blocks/quote'
+import tableField from './blocks/table'
+import textContent from './blocks/textContent'
+import siteMeta from './global/siteMeta'
+
+export default {
+  name: 'blog',
+  type: 'document', // change this to 'object'
+  title: 'Blog Posts',
+  groups: [
+    {
+      name: 'meta',
+      title: 'Page Meta',
+    },
+    {
+      name: 'content',
+      title: 'Content',
+      default: true,
+    },
+  ],
+  fields: [
+    {...siteMeta, title: 'Meta Data', group: ['meta']},
+    {
+      name: 'title',
+      type: 'string',
+      title: 'Title',
+      validation: (Rule) => Rule.required(),
+      group: ['content'],
+    },
+    {
+      name: 'blogFeaturedImage',
+      title: 'Featured Image',
+      type: 'image',
+      group: ['content'],
+    },
+    {
+      name: 'blogIntro',
+      title: 'Intro Text',
+      type: 'text',
+      group: ['content'],
+    },
+    {
+      title: 'Content Section',
+      name: 'blogContent',
+      type: 'array',
+      of: [blockContent, image, textContent, quote, accordion, tableField],
+      group: ['content'],
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      hidden: true,
+    },
+  ],
+  preview: {
+    select: {
+      title: 'title',
+    },
+  },
+}
--- schemas/index.ts
+++ schemas/index.ts
@@ -1,6 +1,7 @@
+import blog from './blog'
 import homePage from './homepage'
 import pages from './pages'
 import resourcePages from './resourcePages'
 import resources from './resources'
 import siteSettings from './siteSettings'
-export const schemaTypes = [siteSettings, homePage, pages, resources, resourcePages]
+export const schemaTypes = [siteSettings, homePage, pages, blog, resources, resourcePages]
